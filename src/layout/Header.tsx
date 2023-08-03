42d716c57257665c1e0b721d71d9ce420e2ec9d0
Thanh Nguyen <thanh@midoriki.com>
2023-08-03 20:51:48 +0700

    fix: responsive header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,12 @@
-import { Text, Flex, Group, Header as MantineHeader, Center } from '@mantine/core';
+import { Flex, Group, Header as MantineHeader, Center } from '@mantine/core';
 import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
 import LoginLink from '@layout/LoginLink';
 import RegisterLink from '@layout/RegisterLink';
 import { useProfileStore } from '@lib/stores/ProfileStore';
 import Avatar from './Avatar';
+import { useMediaQuery } from '@mantine/hooks';
+import { IconHome } from '@tabler/icons-react';
 
 const LinkWrapper = styled(Link)`
   text-decoration: none;
@@ -14,10 +16,14 @@ const LinkWrapper = styled(Link)`
 export default function Header () {
   const user = useProfileStore((state) => state.user);
 
+  const matches = useMediaQuery('(max-width: 530px)');
+
   return (
     <MantineHeader height={60}>
       <Group sx={{ height: '100%' }} px={20} position="apart">
-        <LinkWrapper to={'/'} style={{ fontSize: '30px' }}>Funny Youtube Videos</LinkWrapper>
+        <LinkWrapper to={'/'} style={{ fontSize: '30px' }}>
+          {matches ? (<IconHome size="30px" />) : 'Funny Youtube Videos'}
+        </LinkWrapper>
         <Flex direction="row" gap="md">
           {user ? (
             <>
