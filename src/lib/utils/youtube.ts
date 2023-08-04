cbb509ac907b1a0636254924497c0651f6463764
Thanh Nguyen <thanh@midoriki.com>
2023-08-04 15:49:36 +0700

    fix: update youtube video extractor

--- src/lib/utils/youtube.ts
+++ src/lib/utils/youtube.ts
@@ -1,9 +1,9 @@
 export function extractVideoIdFromURL (url: string): string {
   let matcher;
   if (url.includes('youtube.com')) {
-    matcher = new RegExp(/youtube\.com.*v=(\w+)/);
+    matcher = new RegExp(/youtube\.com.*v=([\w\-|_]+)/);
   } else if (url.includes('youtu.be')) {
-    matcher = new RegExp(/youtu\.be\/(\w+)/);
+    matcher = new RegExp(/youtu\.be\/([\w\-\_]+)/);
   }
 
   if (!matcher) {
